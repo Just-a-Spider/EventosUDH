0e3cc4823eaed9c856c5774efd534bfce7dde0f2
Just-a-Spider <andre_arg_0116@outlook.com>
2024-12-05 05:10:17 -0500

    Remove landing view and related files; update routing and module imports for speakers

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -22,8 +22,12 @@ import { EventDetailComponent } from './components/events/detail/detail.componen
 import { LiveClockComponent } from './components/events/detail/live-clock/live-clock.component';
 import { EventParticipantsComponent } from './components/events/detail/participants/participants.component';
 import { EventSpeakersComponent } from './components/events/detail/speakers/speakers.component';
+import { EditEventForm } from './components/events/edit/edit.component';
 import { EventListComponent } from './components/events/list/list.component';
 import { NewEventForm } from './components/events/new/new.component';
+import { SpeakerDetailComponent } from './components/speakers/detail/detail.component';
+import { SpeakersListComponent } from './components/speakers/list/list.component';
+import { NewComponent } from './components/speakers/new/new.component';
 import { HeaderComponent } from './components/UI/header/header.component';
 import { NotificationsComponent } from './components/UI/header/notifications/notifications.component';
 import { SideBarComponent } from './components/UI/side-bar/side-bar.component';
@@ -31,13 +35,8 @@ import { apiKeyInterceptor } from './interceptors/api-key.interceptor';
 import { TranslateRolePipe } from './pipes/translate-role.pipe';
 import { AuthView } from './views/auth/auth.component';
 import { HomeView } from './views/home/home.component';
-import { LandingView } from './views/landing/landing.component';
 import { ProfileView } from './views/profile/profile.component';
 import { ResetPasswordView } from './views/reset-password/reset-password.component';
-import { EditEventForm } from './components/events/edit/edit.component';
-import { SpeakersListComponent } from './components/speakers/list/list.component';
-import { SpeakerDetailComponent } from './components/speakers/detail/detail.component';
-import { NewComponent } from './components/speakers/new/new.component';
 
 @NgModule({
   declarations: [
@@ -50,7 +49,6 @@ import { NewComponent } from './components/speakers/new/new.component';
     AuthView,
     HomeView,
     ProfileView,
-    LandingView,
 
     // Components
     SideBarComponent,
