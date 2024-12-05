0612a715a9df25358d53a52100e2a4cf6092c46d
Just-a-Spider <andre_arg_0116@outlook.com>
2024-12-05 05:10:17 -0500

    Remove landing view and related files; update routing and module imports for speakers

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EventListComponent } from './components/events/list/list.component';
+import { SpeakersListComponent } from './components/speakers/list/list.component';
 import { authGuard } from './guards/auth.guard';
 import { AuthView } from './views/auth/auth.component';
 import { HomeView } from './views/home/home.component';
-import { LandingView } from './views/landing/landing.component';
 import { ProfileView } from './views/profile/profile.component';
 import { ResetPasswordView } from './views/reset-password/reset-password.component';
-import { SpeakersListComponent } from './components/speakers/list/list.component';
 
 const routes: Routes = [
-  { path: 'landing', component: LandingView },
   { path: 'auth', component: AuthView },
   { path: 'reset-password', component: ResetPasswordView },
   {
